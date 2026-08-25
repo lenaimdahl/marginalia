8de0ba82ea09cf32d5341123846d5a696b6a5895
Lena Imdahl <mail@lenaimdahl.de>
2026-08-25 20:33:58 +0200

    fix type

--- packages/frontend/src/hooks/useCreateBook.ts
+++ packages/frontend/src/hooks/useCreateBook.ts
@@ -1,7 +1,8 @@
-import type { Book } from "../components/CreateBook";
+import type { Book } from "../types/generated/book";
+import type { CreateBookInput } from "../components/CreateBook";
 
 export function useCreateBook() {
-  async function createBook(book: Book) {
+  async function createBook(book: CreateBookInput): Promise<Book> {
     const response = await fetch("http://localhost:8080/books", {
       method: "POST",
       headers: {
