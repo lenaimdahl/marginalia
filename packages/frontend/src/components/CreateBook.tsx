a0e0814db6e0aaf341a542a7eaf6b23a73824678
Lena Imdahl <mail@lenaimdahl.de>
2026-08-26 11:10:57 +0200

    add missing input fields

--- packages/frontend/src/components/CreateBook.tsx
+++ packages/frontend/src/components/CreateBook.tsx
@@ -1,26 +1,47 @@
 import { useState } from "react";
-import type { Book } from "../types/generated/book";
+import type { Book, Status } from "../types/generated/book";
 
-export type CreateBookInput = Pick<Book, "title" | "author">;
+export type CreateBookInput = Pick<
+  Book,
+  "title" | "author" | "status" | "notes"
+>;
+
+type EditableBookField = keyof CreateBookInput;
 
 type CreateBookProps = {
   onSave: (book: CreateBookInput) => Promise<void>;
 };
 
 export function CreateBook({ onSave }: CreateBookProps) {
-  const [book, setBook] = useState<CreateBookInput>({ title: "", author: "" });
+  const [book, setBook] = useState<CreateBookInput>({
+    title: "",
+    author: "",
+    status: "will-read",
+    notes: "",
+  });
+
+  function handleChangeBook(field: EditableBookField, value: string) {
+    setBook(
+      (currentBook) =>
+        ({
+          ...currentBook,
+          [field]: value,
+        }) as CreateBookInput,
+    );
+  }
 
   async function handleSubmit(event: { preventDefault: () => void }) {
     event.preventDefault();
 
     const title = book.title.trim();
     const author = book.author.trim();
+    const notes = book.notes?.trim();
     if (!title || !author) {
       return;
     }
 
-    await onSave({ title, author });
-    setBook({ title: "", author: "" });
+    await onSave({ title, author, status: book.status, notes });
+    setBook({ title: "", author: "", status: "will-read", notes: "" });
   }
 
   return (
@@ -29,13 +50,29 @@ export function CreateBook({ onSave }: CreateBookProps) {
         type="text"
         placeholder="Buchtitel"
         value={book.title}
-        onChange={(event) => setBook({ ...book, title: event.target.value })}
+        onChange={(event) => handleChangeBook("title", event.target.value)}
       />
       <input
         type="text"
         placeholder="Autor"
         value={book.author}
-        onChange={(event) => setBook({ ...book, author: event.target.value })}
+        onChange={(event) => handleChangeBook("author", event.target.value)}
+      />
+      <select
+        value={book.status}
+        onChange={(event) =>
+          handleChangeBook("status", event.target.value as Status)
+        }
+      >
+        <option value="will-read">Möchte ich lesen</option>
+        <option value="reading">Lese ich gerade</option>
+        <option value="finished">Gelesen</option>
+        <option value="quit">Abgebrochen</option>
+      </select>
+      <textarea
+        placeholder="Notizen"
+        value={book.notes}
+        onChange={(event) => handleChangeBook("notes", event.target.value)}
       />
       <button type="submit">Buch speichern</button>
     </form>
--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import type { Book } from "../types/generated/book";
 import { useCreateBook } from "../hooks/useCreateBook";
 import { CreateBook, type CreateBookInput } from "../components/CreateBook";
+import { useTranslation } from "react-i18next";
 
 function Home() {
+  const { t } = useTranslation();
   const [books, setBooks] = useState<Book[]>([]);
   const { createBook } = useCreateBook();
 
@@ -15,7 +17,8 @@ function Home() {
 
   return (
     <div>
-      <h1>Hallo Gib einen Buchtitel ein</h1>
+      <h1>{t("pages.home.title")}</h1>
+      <h2>{t("pages.home.subTitle")}</h2>
       <CreateBook onSave={saveBook} />
       <ul>
         {books.map((book, index) => (
